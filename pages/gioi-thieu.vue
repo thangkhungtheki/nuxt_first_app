c798e8e3303e8495917c34129b4d05d14daacc2e
thangkhungtheki <thangkhungtheki@gmail.com>
2023-11-30 09:47:01 +0700

    thêm coponent list

--- pages/gioi-thieu.vue
+++ pages/gioi-thieu.vue
@@ -2,30 +2,37 @@
   <div>
     <b-breadcrumb :items="items"></b-breadcrumb>
     <player></player>
+    <Chapagination></Chapagination>
   </div>
   </template>
   
   <script>
   import player from "~/components/jwplayer.op.vue"
+  import Chapagination from "../components/chapagination.vue";
     export default {
-      player,
-      data() {
+    components : {
+      Chapagination,
+    },
+    player,
+    
+    data() {
         return {
-          items: [
-            {
-              text: 'Trang chủ',
-              href: '#'
-            },
-            {
-              text: 'Nội dung',
-              href: '#'
-            },
-            {
-              text: 'Thư viện',
-              active: true
-            }
-          ]
-        }
-      }
-    }
+            items: [
+                {
+                    text: 'Trang chủ',
+                    href: '#'
+                },
+                {
+                    text: 'Nội dung',
+                    href: '#'
+                },
+                {
+                    text: 'Thư viện',
+                    active: true
+                }
+            ]
+        };
+    },
+    components: { Chapagination }
+}
   </script>
